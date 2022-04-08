4679fddd3adc0563a18f6f2cd931a6435b1204d0
Colette Smith <colette11295@gmail.com>
2022-04-08 14:41:43 -0500

    Fix minor bug

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,15 +1,9 @@
-
-
-
 export default function Dropdown(props) {
-
-    function onClick(e) => 
-    
     return (
-    <div>
-      {props.users.map((user) => (
-        <button onClick={() => console.log("/" + user)}>{user}</button>
-      ))}
-    </div>
-  );
+        <div>
+            {props.users.map((user) => (
+                <button onClick={() => console.log('/' + user)}>{user}</button>
+            ))}
+        </div>
+    );
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,20 @@
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
-import Navigation from "./Navigation";
+import styles from '../styles/Home.module.css';
+import Head from 'next/head';
+import Navigation from './Navigation';
 
 export default function Layout(props) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Colette Smith</title>
-        <meta name="description" content="Colette's Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <a href="/" className={styles.title}>
-          Yesipe
-        </a>
-        <Navigation />
-        {props.children}
-      </main>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Colette Smith</title>
+                <meta name="description" content="Colette's Portfolio" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.title}>Yesipe</div>
+                <Navigation />
+                {props.children}
+            </main>
+        </div>
+    );
 }
