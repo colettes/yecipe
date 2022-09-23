2ed393c45c6303a56a8f4f50349828db944a8a4c
Colette Smith <colette11295@gmail.com>
2022-09-22 23:43:05 -0500

    Modify dropdown component

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,9 +1,19 @@
 export default function Dropdown(props) {
     return (
         <div>
-            {props.users.map((user) => (
+            <select
+                name="userList"
+                id="userList"
+                onChange={(e) => console.log(userList.value)}
+            >
+                {props.users.map((user) => (
+                    <option value={user}>{user}</option>
+                ))}
+            </select>
+
+            {/* {props.users.map((user) => (
                 <button onClick={() => console.log('/' + user)}>{user}</button>
-            ))}
+            ))} */}
         </div>
     );
 }
